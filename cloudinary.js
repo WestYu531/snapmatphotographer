6b08d295ad81db2e5a7e22694d2a9d69902301f9
Zhongtian Yu <ztyu@ucdavis.edu>
2025-05-20 21:33:01 -0700

    cloudiary里面的cloudname修改了

--- cloudinary.js
+++ cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'snapmate', // 替换为你的cloud_name
+  cloud_name: 'dtiuetopn', // 替换为你的cloud_name
   api_key: '451666656835754',      // 替换为你的api_key
   api_secret: 'oxBQjhqUC7qM7K6_kvCRPNr8p6U' // 替换为你的api_secret
 });
